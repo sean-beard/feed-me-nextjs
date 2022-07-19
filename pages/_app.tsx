8633608031f7c18d0c7eba445829c93ce0240964
Sean Beard <sean61793@my.uri.edu>
2022-07-19 08:49:17 -0600

    ui: set up routing and basic layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,58 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import Link from "next/link";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>FeedMe</title>
+        <meta name="description" content="Personalized newsfeed app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          padding: "2rem",
+        }}
+      >
+        <nav
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <h1>
+            <a href="/">FeedMe</a>
+          </h1>
+
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <ul style={{ listStyle: "none" }}>
+              <li>
+                <Link href="/manage">Manage Feeds</Link>
+              </li>
+            </ul>
+
+            <a style={{ marginLeft: "1rem" }} href="#">
+              Login with Github
+            </a>
+          </div>
+        </nav>
+
+        <main>
+          <Component {...pageProps} />
+        </main>
+
+        <footer>This is the footer</footer>
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/manage.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Manage: NextPage = () => {
+  return <div>This is the manage feeds page.</div>;
+};
+
+export default Manage;
